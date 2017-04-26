a79eb3f2539bb74e3f0f6f25f1a5fa54d2ad57c8
Kimmo Koskenniemi <koskenni@LM6-NKIEL4.local>
2017-04-26 20:56:53 +0300

    Release 0.1 which appears to have the basic functionalities - please report any bugs
    Committer: Kimmo Koskenniemi <kimmo.koskenniemi@helsinki.fi>

--- beta.py
+++ beta.py
@@ -1,52 +1,93 @@
+# beta.py
+#
+# Copyright 2017 by Kimmo Koskenniemi
+#
+# Reimplementation of the Beta string rewriting engine originally
+# written by Benny Brodda in Fortran and later on reimplemented by
+# Kimmo Koskenniemi in Pascal and in C.
+# The input formalism follows the C version as documented in
+# F. Karlsson and K. Koskenniemi, "Beta-ohjelma kielentutkimuksen apuvälineenä",
+# Yliopistopaino, 1990.
+#
+# This program was written from scratch in Python3 without any
+# reference to the above mentioned predecessors.
+#
+# This program is free software under the GPL 3 license
+# Version 0.1 - 2017-04-26
+#
 from collections import deque
 import string
 import datrie
 
-trie = datrie.Trie(string.ascii_lowercase + "äö")
+euroletters = "´áÁćĆéÉíÍĺĹńŃóÓŕŔśŚúÚẃẂýÝźŹǽǼǿǾǻǺ˘ăĂĕĔğĞĭĬŏŎŭŬˇǎǍčČďĎěĚǧǦȟȞǐǏǩǨľĽňŇǒǑřŘšŠťŤǔǓžŽǯǮ¸çÇģĢķĶļĻņŅŗŖşŞţŢâÂĉĈêÊĝĜĥĤîÎĵĴôÔŝŜûÛŵŴŷŶ¨äÄëËïÏöÖüÜẅẄÿŸ˙ḃḂċĊḋḊėĖḟḞġĠİṁṀṗṖṡṠṫṪżŻạẠẹẸịỊọỌụỤỵỴ˝őŐűŰàÀèÈìÌòÒùÙẁẀỳỲ¯āĀēĒīĪōŌūŪǣǢǟǞ˛ąĄęĘįĮǫǪųŲ˚åÅůŮãÃẽẼĩĨñÑõÕũŨỹỸđĐǥǤħĦłŁøØŧŦắặằẳẵẮẶẰẲẴấậầẩẫẤẬẦẨẪếệềểễỆỆỀỂỄốộồổỗỐỘỒỔỖǟǞȧǡȦǠảẢẻẺỉỈỏỎủỦỷỶơớợờờỡƠỚỢỜỞỠưứựừửữƯỨỰỪỬỮǭǬǻǺƒﬁﬂĳĲŀĿŉɼſẛẛșȘțȚ"
+
+characters = string.printable + euroletters
+allowed_characters = set(characters)
+
+trie = datrie.Trie(characters)
 chset = {}
 stset = {}
 
-def betaproc(line, verbosity):
+def betaproc(line, max_cycles, verbosity):
     global trie, chset, stset
+    cycles = 0
     items = deque()
-    items.appendleft(("##", line + "##", 0))
-    while len(items) > 0:
+    item = ("##", line + "##", 1)
+    item_set = {item}
+    items.appendleft(item)
+    while len(items) > 0:                              # item loop
+        cycles += 1
+        if cycles > max_cycles:
+            print("** possibly an infinite loop, stopped at", max_cycles)
+            return
         left, right, state = items.pop()
         if verbosity > 0:
             print(left + " >>> " + right + " -- " + str(state))
-        if len(right) == 0 and len(left) > 0:
-            print(left[2:-2])
-            continue
+        if len(right) <= 1 and len(left) >= 2:
+            print(left[2:-1])
+            continue                         # nothin more for this item
         rule_items = trie.prefix_items(right)
-        if verbosity > 11:
-            print(rule_items)
-        for x, rule_list in rule_items:
-            for rule in rule_list:
-                if verbosity > 10:
-                    print("testing for", rule)
+        item_list = []
+        for x in rule_items:
+            item_list.append(x)
+        if verbosity >= 10:
+            print("list of prefix items:", item_list)
+        for x, rule_list in reversed(item_list):       # prefix loop 
+            for rule in rule_list:                     # rule loop
+                if verbosity >= 10:
+                    print("testing for rule:", rule)
                 y, lc, rc, sc, ns, mv, md = rule
-                if lc != '0' and lc != '':
-                    # print("testing that", left[-1], "is in", chset[lc]) ##
-                    if left[-1] not in chset[lc]:
-                        continue
-                    else:
-                        if verbosity > 10:
-                            print("left conditon tested and satisfied")
-                if rc != '0' and rc != '':
-                    if right[len(x)] not in chset[rc]:
-                        continue
-                    else:
-                        if verbosity > 10:
-                            print("right conditon tested and satisfied")
-                if sc != '0' and sc != '':
-                    if state not in stsets[sc]:
-                        continue
-                    else:
-                        if verbosity > 10:
-                            print("state conditon tested and satisfied")
-                if mv == 1:
+                if lc != '0':
+                    if lc[0] == '-':
+                        if left[-1] in chset[lc[1:]]:
+                            continue         # no more tests for this rule
+                    elif left[-1] not in chset[lc]:
+                        continue             # no more tests for this rule
+                    if verbosity >= 10:
+                        print("left conditon tested and satisfied")
+                if rc != '0':
+                    if rc[0] == '-':
+                        if right[len(x)] in chset[rc[1:]]:
+                            continue         # no more tests for this rule
+                    elif right[len(x)] not in chset[rc]:
+                        continue             # no more tests for this rule
+                    if verbosity >= 10:
+                        print("right conditon tested and satisfied")
+                if sc != '0':
+                    if sc[0] == '-':
+                        if state in stset[sc[1:]]:
+                            continue         # continue with next rule
+                    elif state not in stset[sc]:
+                            continue         # continue with next rule
+                    if verbosity >= 10:
+                        print("state conditon tested and satisfied")
+                # all tests passed for this rule
+                if mv == 0:             # delete this item
                     left1 = ""
-                    right1 = left + y + right[len(x):]
+                    right1 = ""
+                elif mv == 1:           # move to the beginning
+                    left1 = "##"
+                    right1 = left[2:] + y + right[len(x):]
                 elif mv == 2:
                     left1 = left[:-1]
                     right1 = left[-1:] + y + right[len(x):]
@@ -55,35 +96,52 @@ def betaproc(line, verbosity):
                     right1 = y + right[len(x):]
                 elif mv == 4:
                     left1 = left + y[:-1]
-                    right1 = y[-1] + y + right[len(x):]
+                    right1 = y[-1] + right[len(x):]
                 elif mv == 5:
                     left1 = left + y
                     right1 = right[len(x):]
                 elif mv == 6:
+                    left1 = left + y + right[len(x):-2]
+                    right1 = "##"
+                elif mv == 7:           # accept this item immediately
                     left1 = left + y + right[len(x):]
                     right1 = ""
-                items.appendleft((left1, right1, ns))
-                if verbosity > 9:
+                    print(left1[2:-2])
+
+                if ns < 0:              # neg rs increments
+                    ns = state - ns
+                elif ns == 0:           # zero rs keeps the state
+                    ns = state
+
+                if len(left1) >= 2 and len(right1) >= 2:
+                    items.appendleft((left1, right1, ns))
+                if verbosity >= 10:
                     print((left1, right1, ns), "pushed to the queue")
-                if md != 2:
-                    break
-            else:
-                continue
-            break
-        else:
+                if md != 2:             # non-deterministic rule
+                    break               # leave the 
+            else:                            # all rules for this prefix tested
+                continue                     # to the next prefix
+            break                       # leave the prefix loop
+            #                             (reached by breaking the rule loop)
+        else:                                # all prefixes processed 
             if len(right) > 0:
                 left1 = left + right[0:1]
                 right1 = right[1:]
-                items.appendleft((left1, right1, state))
-                if verbosity > 9:
+                item = (left1, right1, state)
+                if item in item_set:
+                    print("** have been in this configuration before:", item)
+                items.appendleft(item)
+                if verbosity >= 10:
                     print("shifting one step to the right")
     return
 
 import re
 
-def character_sets(*list_of_set_defs):
-    global chset
-    for set_def in list_of_set_defs:
+def character_sets(list_of_set_defs, verbosity):
+    global chset, args
+    for lineno, set_def in list_of_set_defs:
+        if verbosity >= 20:
+            print(lineno, ">>>" + set_def + "<<<") ##
         mat = re.match(r"^\s*(\w+):\s+(.+)\s*$", set_def)
         if mat:
             set_name = mat.group(1)
@@ -91,43 +149,139 @@ def character_sets(*list_of_set_defs):
             symbol_list = re.split(r"\s+", symbol_str)
             chset[set_name] = set(symbol_list)
         else:
-            print(set_def, "** incorrect")
+            print(lineno, ":", set_def, "** incorrect")
 
-def rules(*list_of_rules):
-    global trie
-    par = [0, 0, 0, 0, 5, 1]
-    for rule in list_of_rules:
-        mat = re.match(r"^([^;]*); ([^;]*);(.*)$", rule)
+def state_sets(list_of_set_defs, verbosity):
+    global stset, args
+    for lineno, set_def in list_of_set_defs:
+        if verbosity >= 20:
+            print(lineno, ">>>" + set_def + "<<<") ##
+        mat = re.match(r"^\s*(\w+):\s+(.+)\s*$", set_def)
         if mat:
-            x = mat.group(1)
-            y = mat.group(2)
-            rest = mat.group(3).strip()
-            # print("x:", x, "y:", y, "rest:", rest) ###
-            lst = re.split(r"\s+", rest)
-            par[0:len(lst)] = lst
-            print("par: ", par)
-            new_rule = [y]
-            new_rule.extend(par)
-            print("new rule:", new_rule)
+            set_name = mat.group(1)
+            state_str = mat.group(2)
+            str_list = re.split(r"\s+", state_str)
+            state_list = [int(str) for str in str_list]
+            stset[set_name] = set(state_list)
+        else:
+            print(lineno, ":", set_def, "** incorrect")
+
+def rules(list_of_rules, verbosity):
+    global trie, args
+    lc, rc, sc, rs, mv, md = ('0', '0', '0', 1, 5, 1)
+    pat = re.compile(
+        r"""^(?P<x>(?:[^;]|%[%;])+) # x part
+             ;\s                   # terminates the x part
+             (?P<y>(?:[^%;]|%[%;])*) # y part
+             ;                     # terminates the y part
+             (?:\s+
+                (?P<lc> -? \w+))?  # lc
+             (?:\s+
+                (?P<rc> -? \w+))?  # rc
+             (?:\s+
+                (?P<sc> -? \w+))?  # sc
+             (?:\s+
+                (?P<rs> -? \d+))?  # rs
+             (?:\s+
+                (?P<mv> \d+))?     # mv
+             (?:\s+
+                (?P<md>\d+))?      # md
+             \s*
+             (?:  [(]  [^)]*  [)]  )?  # comment in round parentheses
+             \s*
+           $""",
+        re.X)
+    for lineno, rule in list_of_rules:
+        mat = pat.match(rule)
+        if mat:
+            gd = mat.groupdict()
+            if verbosity >= 20:
+                print(gd)
+            if gd['x']:
+                x = gd['x']
+            else:
+                print("** no valid x part on line", linenum, line)
+                exit()
+            if gd['y'] or gd['y'] == "":
+                y = gd['y']
+            else:
+                print("** no valid y part on line", linenum, line)
+                exit()
+            if gd['lc']:
+                lc = gd['lc']
+            if gd['rc']:
+                rc = gd['rc']
+            if gd['sc']:
+                sc = gd['sc']
+            if gd['rs']:
+                rs = int(gd['rs'])
+            if gd['mv']:
+                mv = int(gd['mv'])
+            if gd['md']:
+                md = int(gd['md'])
+
+            new_rule = (y, lc, rc, sc, rs, mv, md)
+            if verbosity >= 20:
+                print("new rule:", x, new_rule)
             if x in trie:
                 list_of_rules = trie[x]
-                print("trie[x]", x, list_of_rules)
+                if verbosity >= 20:
+                    print("trie[x]", x, list_of_rules)
             else:
                 list_of_rules = []
             list_of_rules.append(tuple(new_rule))
-            print("list_of_rules:", list_of_rules)
+            # print("list_of_rules:", list_of_rules) ##
             trie[x] = list_of_rules
         else:
-            print("error in", rule)
+            print("error on line", lineno, rule)
+    return
 
-if __name__ == "__main__":
-    import argparse
-    arpar = argparse.ArgumentParser("python3 beta.py")
-    arpar.add_argument("-v", "--verbosity",
-                       help="level of diagnostic output",
-                       type=int, default=0)
-    args = arpar.parse_args()
-
+def read_beta_grammar(file_name, verbosity):
+    global chset, stset, trie
+    f = open(file_name, 'r')
+    state = "start"
+    lineno = 0
+    for line in f:
+        line = line.strip()
+        lineno += 1
+        # print(lineno, state, ">>" + line + "<<") ##
+        if len(line) == 0 or line[0] == '!':
+            continue
+        if state == "start" and line == "CHARACTER-SETS":
+            list_of_char_sets = []
+            state = "chsets"
+            continue
+        elif state in {"start", "chsets"} and line == "STATE-SETS":
+            list_of_state_sets = []
+            state = "stsets"
+            continue
+        elif state in {"start", "chsets", "stsets"} and line == "RULES":
+            list_of_rules = []
+            state = "rules"
+            continue
+        elif state == "chsets":
+            list_of_char_sets.append((lineno, line))
+            # print(list_of_char_sets)
+        elif state == "stsets":
+            list_of_state_sets.append((lineno, line))
+        elif state == "rules":
+            list_of_rules.append((lineno, line))
+        else:
+            print("*** missing keyword on line", lineno)
+            print(line)
+            exit()
+    # print("-- parsing character sets") ##
+    character_sets(list_of_char_sets, verbosity)
+    if verbosity >= 10:
+        print(chset)
+    # print("parsing state sets")
+    state_sets(list_of_state_sets, verbosity)
+    if verbosity >= 10:
+        print(stset)
+    # print("-- parsing rules")
+    rules(list_of_rules, verbosity)
+    
+def testing():
     character_sets(
         'V: a e i o u y ä ö',
         'C: d f g h j k l m n p r s t v'
@@ -144,8 +298,22 @@ if __name__ == "__main__":
         print(chset)
         for item in trie.items(''):
             print(item)
+    return
 
+if __name__ == "__main__":
+    import argparse
+    arpar = argparse.ArgumentParser("python3 beta.py")
+    arpar.add_argument("-v", "--verbosity",
+                       help="level of diagnostic output",
+                       type=int, default=0)
+    arpar.add_argument("-m", "--max_loops",
+                       help="maximum number of cycles per one input line",
+                       type=int, default=1000)
+    arpar.add_argument("-r", "--rules", help="rule file")
+    args = arpar.parse_args()
+
+    read_beta_grammar(args.rules, args.verbosity)
     import sys
     for line in sys.stdin:
-        betaproc(line.strip(), args.verbosity)
+        betaproc(line.strip(), args.max_loops, args.verbosity)
 
